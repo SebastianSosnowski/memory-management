a1bd634b243894fe0feb75d2c5fa9837c7044b1c
Sebastian Sosnowski <mefiu672@gmail.com>
2026-04-18 19:12:54 +0200

    Fix issue

--- homework/resourceD/resourceD.cpp
+++ homework/resourceD/resourceD.cpp
@@ -1,40 +1,34 @@
 #include <iostream>
+#include <memory>
 #include <stdexcept>
 
 using namespace std;
 
-class Resource
-{ 
-public:
-    void use(const char* N)
-    {
+class Resource {
+  public:
+    void use(const char *N) {
         cout << "Using resource. Passed " << *N << endl;
-        if (*N == 'd')
-        {
+        if (*N == 'd') {
             throw logic_error("Passed d. d is prohibited.");
         }
     };
 };
 
-int main(int argc, char* argv[])
-{
-    if(argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         cerr << "You need to pass 1 argument" << endl;
         exit(-1);
     }
-    const char* N = argv[1];
-    Resource* rsc = nullptr;
-    try
-    {
-        rsc = new Resource();
+    const char *N = argv[1];
+    // Resource *rsc = nullptr;
+    unique_ptr<Resource> rsc;
+    try {
+        rsc = make_unique<Resource>();
         rsc->use(N);
-        delete rsc;
-    }
-    catch (logic_error & e)
-    {
+        // delete rsc;
+    } catch (logic_error &e) {
         cout << e.what() << endl;
+        // delete rsc;
     }
     return 0;
 }
-
